9c99f8148b5ee4abb2b9c752079d5ce6d2674c48
creatureslsp <309397444+creatureslsp@users.noreply.github.com>
2023-12-25 09:52:56 -0800

    build(server): configure browser bundle target in webpack

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -11,30 +11,23 @@
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 const path = require('path');
-const webpack = require('webpack');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 /** @type WebpackConfig */
-const webExtensionConfig = {
+const shared = {
     mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-    target: 'webworker', // extensions run in a webworker context
     entry: {
         server: './src/server.ts', // source of the web extension main file
         // 'test/suite/index': './src/web/test/suite/index.ts', // source of the web extension test runner
     },
     output: {
         filename: '[name].js',
-        path: path.join(__dirname, './dist/web'),
-        libraryTarget: 'umd',
+        libraryTarget: 'commonjs',
         devtoolModuleFilenameTemplate: '../../[resource-path]'
     },
     resolve: {
         mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
         extensions: ['.ts', '.js'], // support ts-files and js-files
-        alias: {
-            'vscode-languageserver/node': 'vscode-languageserver/browser',
-            [path.resolve(__dirname, "./src/connection.vscode.ts")]: path.resolve(__dirname, "./src/connection.web.ts"),
-        },
         fallback: {
             // Webpack 5 no longer polyfills Node.js core modules automatically.
             // see https://webpack.js.org/configuration/resolve/#resolvefallback
@@ -55,14 +48,6 @@ const webExtensionConfig = {
             },
         ],
     },
-    plugins: [
-        new NodePolyfillPlugin({
-            excludeAliases: ['console']
-        }),
-        new webpack.ProvidePlugin({
-            process: 'process/browser' // provide a shim for the global `process` variable
-        })
-    ],
     externals: {
         vscode: 'commonjs vscode', // ignored because it doesn't exist
     },
@@ -72,4 +57,48 @@ const webExtensionConfig = {
     devtool: 'nosources-source-map', // create a source map that points to the original source file
 };
 
-module.exports = [webExtensionConfig];
+/** @type WebpackConfig */
+const webExtensionConfig = {
+    ...shared,
+    target: 'webworker', // extensions run in a webworker context
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, './dist/web'),
+        libraryTarget: 'commonjs',
+    },
+    resolve: {
+        mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
+        extensions: ['.ts', '.js'], // support ts-files and js-files
+
+        fallback: {
+            // Webpack 5 no longer polyfills Node.js core modules automatically.
+            // see https://webpack.js.org/configuration/resolve/#resolvefallback
+            // for the list of Node.js core module polyfills.
+            assert: require.resolve('assert'),
+        },
+    },
+    plugins: [
+        new NodePolyfillPlugin({
+            excludeAliases: ['console']
+        })
+    ],
+};
+
+webExtensionConfig.resolve.alias = {
+    'vscode-languageclient/node': 'vscode-languageclient/browser',
+    [path.resolve(__dirname, "./src/connection.vscode.ts")]: path.resolve(__dirname, "./src/connection.web.ts"),
+};
+
+
+/** @type WebpackConfig */
+const nodeExtensionConfig = {
+    ...shared,
+    target: 'node', // extensions run in a webworker context
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, './dist/node'),
+        libraryTarget: 'commonjs',
+    },
+};
+
+module.exports = [webExtensionConfig, nodeExtensionConfig];
